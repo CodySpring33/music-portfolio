2a6bdae3a63ce7cdfb0695efb1b17ef3f8b7f00e
Cjspring <cjspring44@gmail.com>
2024-11-09 11:30:54 -0800

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   transform: {
-    '^.+\\.vue$': 'vue-jest'
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.jsx?$': 'babel-jest'
   },
-  testMatch: [
-    '**/tests/unit/**/*.spec.[jt]s?(x)',
-    '**/__tests__/*.[jt]s?(x)'
-  ]
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: ["node", "node-addons"],
+  }
 }
